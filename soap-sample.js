ff5db1a2274080fbd1ad8f50217e76a1f0e1b4b4
Amirhossein Mehrvarzi <36813422+amhome@users.noreply.github.com>
2022-06-19 15:53:55 +0430

    SOAP fix

--- soap-sample.js
+++ soap-sample.js
@@ -4,6 +4,6 @@ const SoapClient = require('./lib/soap');
 const soapClient = new SoapClient('username', 'password');
 
 soapClient.GetCredit().then(res => console.log(res));
-
-soapClient.SendSimpleSMS2('09123456789', '5000xxx', 'test sms', false)
-    .then(res => console.log(res));
+// soapClient.GetOutBoxCount().then(res => console.log(res));
+// soapClient.SendSimpleSMS2('09123456789', '5000xxx', 'test sms', false)
+//     .then(res => console.log(res));
